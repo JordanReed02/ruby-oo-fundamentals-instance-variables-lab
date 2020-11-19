ae0063342a2de9415165ac186bef8bfa0f0388f1
Joran <jordanalireed@icloud.com>
2020-11-19 01:29:34 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,17 @@
 class Dog
-  lassie = Dog.new
-  def name=(dog_name)
-    this_dogs_name = dog_name
+
+  @this_dogs_name = 'Lassie'
+
+  lassie = Dog.new #=> #
+  def name=(lassie)
+    @this_dogs_name = 'Lassie'
   end
 
   def name
-    this_dogs_name = 'Lassie'
+     @this_dogs_name = 'Lassie'
   end
+end
 
 lassie = Dog.new
 lassie.name #=> "Lassie"
+lassie.object_id #=> 2
